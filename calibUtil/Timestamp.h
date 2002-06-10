ec7fe6646bd0b29ee804ed00bf10fa3dae5da77c
jrb <>
2002-06-10 18:59:23 +0000

    Use MySQL-style string represenation of timestamp

--- calibUtil/Timestamp.h
+++ calibUtil/Timestamp.h
@@ -1,4 +1,4 @@
-// $Header: $
+// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/calibUtil/Timestamp.h,v 1.1 2002/06/08 22:46:59 jrb Exp $
 #ifndef CALIBUTIL_TIMESTAMP_H
 #define CALIBUTIL_TIMESTAMP_H
 
@@ -9,9 +9,9 @@ namespace calibUtil {
       interval.  Resolution is a second.  Provide conversion to/from a 
       format that MySQL will accept for a datetime and provide comparison.
 
-      Support string format like this:
+      Support the string format used for MySQL DATETIME type:
 
-           yyyy:mm:dd:hh:mm:ss
+           yyyy-mm-dd hh:mm:ss
 
                           where 0 < mm < 13
                                 0 < dd < 32
@@ -34,54 +34,48 @@ namespace calibUtil {
     /// Count seconds from the creation-according-to-unix, somewhere in 1970
     Timestamp(double seconds);
 
-    static const char DEFAULT_DELIM = ':';
-    /** Create a timestamp from an ascii string with fields delimited by
-        a one-character delimiter.  Fields, all represented with decimal
-        strings, must start with year and decrease in significance.  Trailing
-        fields after day may be omitted.  With default delimiter, valid
-        input string look like 
-               "2002:9:8"     (Sept. 8, 2002) or
-               "2003:5:06:10" (May 6, 2003, 10 AM)
+    /** Create a timestamp from an ascii string of standard form
+                yyyy-mm-dd hh:mm:ss
+        where only the first three fields are required.   
+
         If the string is invalid, object will represent unix creation time.
 
         Assume for now -- and preferably forever -- that all times are GMT.
     */
-    Timestamp(const std::string& str, char delim = DEFAULT_DELIM);
+    Timestamp(const std::string& str);
 
     Timestamp(unsigned int year, unsigned int month, unsigned int day, 
               unsigned int hour = 0, unsigned int minute = 0, 
               unsigned int second = 0);
 
-    std::string Timestamp::ascii(char delim = DEFAULT_DELIM);
+    std::string& timeString() {return m_strTime;}
 
-    bool Timestamp::operator<(const Timestamp& other) {
+    bool operator<(const Timestamp& other) {
       return m_time < other.m_time;
     }
 
-    bool Timestamp::operator>(const Timestamp& other) {
+    bool operator>(const Timestamp& other) {
       return m_time > other.m_time;
     }
 
-    bool Timestamp::operator==(const Timestamp& other) {
+    bool operator==(const Timestamp& other) {
       return m_time == other.m_time;
     }
 
-    bool Timestamp::operator!=(const Timestamp& other) {
+    bool operator!=(const Timestamp& other) {
       return m_time != other.m_time;
     }
 
   private:
 
-    static time_t toBinary(const std::string& strTime, 
-                           char delim = DEFAULT_DELIM);
+    static time_t toBinary(const std::string& strTime); 
 
-    static void toString(time_t bin, std::string& strTime, 
-                        char delim = DEFAULT_DELIM);
+    static void toString(time_t bin, std::string& strTime);
 
     /// internal binary rep of time
     time_t m_time;
 
-    /// For convenience also keep ascii rep using default delimiter
+    /// ascii equivalent
     std::string m_strTime;  
   };
 }
--- src/Timestamp.cxx
+++ src/Timestamp.cxx
@@ -1,4 +1,4 @@
-// $Header:  $
+// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/Timestamp.cxx,v 1.1 2002/06/08 22:51:09 jrb Exp $
 
 #include <ctime>
 #include <cstdlib>
@@ -7,7 +7,7 @@
 
 namespace calibUtil {
 
-  Timestamp::Timestamp(double seconds) : m_time(seconds) {
+  Timestamp::Timestamp(double seconds) : m_time((time_t) seconds) {
     toString(m_time, m_strTime);
   }
 
@@ -16,8 +16,8 @@ namespace calibUtil {
     toString(m_time, m_strTime);
   }
 
-  Timestamp::Timestamp(const std::string& str, char delim) {
-    m_time = toBinary(str, delim);
+  Timestamp::Timestamp(const std::string& str) {
+    m_time = toBinary(str);
     toString(m_time, m_strTime);
   }
 
@@ -40,16 +40,20 @@ namespace calibUtil {
     toString(m_time, m_strTime);
   }
 
-  time_t Timestamp::toBinary(const std::string& strTime, char delim) {
+  time_t Timestamp::toBinary(const std::string& strTime) {
     // Break out fields
     struct tm fields;
 
     unsigned int pos;
     unsigned int oldPos = 0;
 
+    // Three delimiter characters may occur in ascii time representation.
+    // First is hyphen.
+    char  delim = '-';
+
     pos = strTime.find(delim, oldPos);
 
-    // Should have at least two occurrences of delim
+    // Must have two occurrences of hyphen delimiter
     if (pos == strTime.size()) return 0;
 
     fields.tm_year = atoi((strTime.substr(oldPos, pos)).c_str()) - 1900;
@@ -66,6 +70,8 @@ namespace calibUtil {
     // day
     oldPos = pos + 1;
 
+    // A space separate time from date (if time is present at all)
+    delim = ' ';
     pos = strTime.find(delim, oldPos);
 
     fields.tm_mday = atoi((strTime.substr(oldPos, pos)).c_str());
@@ -74,6 +80,7 @@ namespace calibUtil {
     fields.tm_hour = fields.tm_min = fields.tm_sec = 0;
 
     if (pos != strTime.size() ) {   // more fields to process
+      delim = ':';
       oldPos = pos + 1;
 
       pos = strTime.find(delim, oldPos);
@@ -99,16 +106,14 @@ namespace calibUtil {
     return timegm(&fields);
   }
   
-  void Timestamp::toString(time_t bin, std::string& strTime, 
-                           char delim) {
+  void Timestamp::toString(time_t bin, std::string& strTime) {
     struct tm * fields = gmtime(&bin);
 
-    std::string dstring(&delim, 1);
     strTime.resize(0);
     std::strstream s;
-    s << fields->tm_year << dstring << fields->tm_mon << dstring;
-    s << fields->tm_mday << dstring << fields->tm_hour << dstring;
-    s << fields->tm_min << dstring << fields->tm_sec;
+    s << (fields->tm_year + 1900) << "-" << (fields->tm_mon + 1) << "-";
+    s << fields->tm_mday << " " << fields->tm_hour << ":";
+    s << fields->tm_min << ":" << fields->tm_sec;
     strTime += s.str();
 
   }
