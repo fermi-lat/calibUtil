2ddf99a14013902f1b107a60ed190f21c477fdc8
madhup <>
2002-06-27 08:49:50 +0000

    New method prototype traverseInfo added

--- calibUtil/StripSrv.h
+++ calibUtil/StripSrv.h
@@ -1,27 +1,32 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/calibUtil/StripSrv.h,v 1.1 2002/06/13 00:06:47 madhup Exp $ 
+// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/calibUtil/StripSrv.h,v 1.2 2002/06/22 00:47:46 madhup Exp $ 
 #ifndef CALIBUTIL_STRIPSRV_H
 #define CALIBUTIL_STRIPSRV_H
 
 #include <string>
 #include <iostream>
 #include <vector>
+
 #include "calibUtil/GenericSrv.h"
+#include "calibUtil/ClientObject.h"
 
 namespace calibUtil {
 
-  typedef struct stowerRC { unsigned int row; unsigned int col;} towerRC; 
-
   class StripSrv {
 
-  public: 
+  private: 
 
     /// this function takes in a stripList in string format and 
     /// fills a vector with corresponding strip numbers
     strToNum(std::string strips, std::vector<unsigned int> &v);
 
+  public:
+
     /// constructor. Initializes strip service by creating a DOM structure 
     /// out the XML file and filling in the internal data structures 
     StripSrv(std::string xmlFileName);
+
+    /// destructor. Deallocate memory
+    ~StripSrv();
   
     /// return the status (Hot or Dead) of the strip
     std::string getBadType();
@@ -43,18 +48,22 @@ namespace calibUtil {
     unsigned int countBad(towerRC towerId, unsigned int trayNum);
     
     /// count for very bad strips for the tower,tray and unilayer  specified 
-    unsigned int countVeryBad(towerRC towerId, unsigned int trayNum, std::string uniLayer);
+    unsigned int countVeryBad(towerRC towerId, unsigned int trayNum, 
+                              uniL uniLayer);
     
     /// count for  bad strips (including very bad) for the tower, tray 
     /// and unilayer specified 
-    unsigned int countBad(towerRC towerId, unsigned int trayNum, std::string uniLayer);
+    unsigned int countBad(towerRC towerId, unsigned int trayNum, 
+                          uniL uniLayer);
 
     /// List all very bad strips with the tower,tray and unilayer  
-    std::vector<unsigned int> getVeryBad(towerRC towerId, unsigned int trayNum, std::string uniLayer);
+    std::vector<unsigned int> getVeryBad(towerRC towerId, 
+                                         unsigned int trayNum, uniL uniLayer);
     
     /// List  bad strips (including very bad) with the tower, tray 
     /// and unilayer specified 
-    std::vector<unsigned int> getBad(towerRC towerId, unsigned int trayNum, std::string uniLayer);
+    std::vector<unsigned int> getBad(towerRC towerId, 
+                                     unsigned int trayNum, uniL uniLayer);
     
     
     /// methods giving access to generic data
@@ -70,6 +79,9 @@ namespace calibUtil {
     
     /// Get format Version
     std::string getFmtVer();
+
+    /// call back method for client to access large data
+    void StripSrv::traverseInfo(ClientObject *client);
 
   private:
 
@@ -93,7 +105,7 @@ namespace calibUtil {
     std::string badType;
 
     /// Contained object to store generic data
-    GenericSrv genSrv;   
+    GenericSrv *genSrv;   
     
 
   };
