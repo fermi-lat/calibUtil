4f81293ced10dd24bb9218c7f2886bafda17cfae
jrb <>
2003-01-29 21:33:33 +0000

    Register some hot and dead strip data sets

--- src/maint/fillmeta_v2r1.sql
+++ src/maint/fillmeta_v2r1.sql
@@ -1,44 +1,36 @@
-##   $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/maint/fillmeta_v2.sql,v 1.1 2002/09/23 19:13:22 jrb Exp $
+##   $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/maint/fillmeta_v2r1.sql,v 1.1 2002/11/18 23:02:43 jrb Exp $
 ## Put some fake data into metadata table
 
 use calib;
 
 insert into metadata_v2r1
- set instrument="BTEM", calib_type="TKR_DeadChan", data_fmt="XML",
- vstart="2000-10-31", vend="2002-11-23",fmt_version="v0r0",
- completion="OK",creator="fillmeta.sql",uid="jrb",
- data_ident="$CALIBUTILROOT/xml/test/phantom1.xml",
+ set instrument="LAT", calib_type="TKR_DeadChan", data_fmt="XML",
+ vstart="2001-10-31", vend="2003-11-23",fmt_version="v2r0",
+ completion="OK",creator="fillmeta.sql",uid="jrb", proc_level="PROD",
+ data_ident="$CALIBUTILROOT/xml/test/testDead1.xml",
  notes="total fantasy";
 
 insert into metadata_v2r1
- set instrument="BTEM", calib_type="TKR_DeadChan", data_fmt="XML",
- vstart="2000-10-31", vend="2001-11-23",fmt_version="v0r0",
- completion="inc",creator="fillmeta.sql",uid="jrb", proc_level="DEV",
- locale="SLAC clean room",
- data_ident="$CALIBUTILROOT/xml/test/phantom2.xml",
+ set instrument="LAT", calib_type="TKR_HotChan", data_fmt="XML",
+ vstart="2000-10-31", vend="2003-1-10 17:00",fmt_version="v2r0",
+ completion="OK",creator="fillmeta.sql",uid="jrb", proc_level="PROD",
+ data_ident="$CALIBUTILROOT/xml/test/testHot.xml",
  notes="total fantasy";
 
 insert into metadata_v2r1
- set instrument="BFEM", calib_type="TKR_HotChan", data_fmt="XML",
- vstart="2000-10-31", vend="2002-11-23",fmt_version="v0r0",
+ set instrument="LAT", calib_type="TKR_HotChan", data_fmt="XML",
+ vstart="2000-10-31", vend="2003-11-23",fmt_version="v2r0",
  flavor="chocolate",
- completion="OK",creator="fillmeta.sql",uid="jrb", proc_level="DEV",
- data_ident="$CALIBUTILROOT/xml/test/phantom3.xml",
+ completion="OK",creator="fillmeta.sql",uid="jrb", proc_level="PROD",
+ data_ident="$CALIBUTILROOT/xml/test/testHotChocolate.xml",
  notes="total fantasy";
 
-insert into metadata_v2r1
- set instrument="BTEM", calib_type="CAL_LightAtt", data_fmt="XML",
- vstart="2000-10-31", vend="2000-11-23",fmt_version="v0r0",
- flavor="chocolate", locale="SLAC test beam",
- completion="OK",creator="fillmeta.sql",uid="jrb", proc_level="PROD",
- data_ident="$CALIBUTILROOT/xml/test/phantom4.xml",
- notes="total fantasy";
 
 insert into metadata_v2r1
- set instrument="BTEM", calib_type="TKR_HotChan", data_fmt="XML",
- vstart="2001-10-31", vend="2003-11-23",fmt_version="v0r0",
+ set instrument="LAT", calib_type="TKR_HotChan", data_fmt="XML",
+ vstart="2003-1-10 14:00", vend="2003-11-23",fmt_version="v2r0",
  completion="OK",creator="fillmeta.sql",uid="jrb", proc_level="PROD",
- data_ident="$CALIBUTILROOT/xml/test/phantom5.xml",
+ data_ident="$CALIBUTILROOT/xml/test/testHot2.xml",
  notes="total fantasy";
 
 
