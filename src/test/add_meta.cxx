a78f6fa359ff770e7b41b66dc7b9cedfac4375b4
jrb <>
2005-02-25 23:48:23 +0000

    Change test programs for consistency with modified Metadata class

--- src/test/add_meta.cxx
+++ src/test/add_meta.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/test/add_meta.cxx,v 1.1 2003/01/13 23:03:25 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/test/add_meta.cxx,v 1.2 2003/01/15 00:19:55 jrb Exp $
 /** @file test_meta.cxx
     Sample program to add record(s) to the MySQL database. For now user
     only gets to specify
@@ -14,148 +14,43 @@
 #include <iostream>
 #include "calibUtil/Metadata.h"
 
-
-void gotIt(unsigned int ser, calibUtil::Metadata::eDataFmt dataFmt,
+void gotIt(unsigned int ser, const std::string& dataFmt,
            const std::string& fmtVersion, const std::string& dataIdent);
-
-// bundle up all the fields which must be specified by caller to
-// make a new record
-typedef struct s_MetaInput {
-  calibUtil::Metadata::eInstrument instr;
-  calibUtil::Metadata::eCalibType  calibType;
-  calibUtil::Metadata::eDataFmt    fmt;
-  calibUtil::Metadata::eCompletion cmpl;
-  calibUtil::Metadata::eLevel      procLevel;
-  std::string           path;
-  std::string           inputDesc;
-  std::string           notes;
-  facilities::Timestamp vstart;
-  facilities::Timestamp vend;
-  std::string           flavor;
-}   MetaInput;
-
-bool getUserInput(MetaInput* inp);
 
 int main(int, char**) {
   using calibUtil::Metadata;
   using facilities::Timestamp;
 
-  Metadata  meta;
+  Metadata  meta("*", "*", "calib_test");
 
-  MetaInput userInput;
-
-  while (getUserInput(&userInput)) {
-    Metadata::eRet ret;
-    // Try to insert a record
-    ret = meta.openRecord(userInput.instr, userInput.calibType,
-                          userInput.fmt, "1.0",
-                          userInput.path,
-                          userInput.cmpl,
-                          userInput.procLevel);
-    if (ret) {
-      std::cerr << "openRecord failed with return value " << (int) ret 
-                << std::endl;
-      return ret;
-    }  
-    ret = 
-      meta.addInputDesc(userInput.inputDesc);
-    if (ret) {
-      std::cerr << "Bad return from addInputDesc: " << (int) ret << std::endl;
-      return ret;
-    }
-    ret = meta.addNotes(userInput.notes);
-    if (ret) {
-      std::cerr << "Bad return from addNotes: " << (int) ret << std::endl;
-      return ret;
-    }
-    ret = meta.addValidInterval(userInput.vstart, userInput.vend);
-    if (ret) {
-      std::cerr << "Bad return from addValidInterval: " << (int) ret 
-                << std::endl;
-      return ret;
-    }
-    
-    ret = meta.addFlavor(userInput.flavor);
-    
-    unsigned int newSerial;
-    ret = meta.insertRecord(&newSerial);
-    if (ret) {
-      std::cerr << "Bad return from insertRecord: " << (int) ret << std::endl;
-    }
-    else {
-      std::cout << "Successfully inserted new record, serial number " 
-                << newSerial << std::endl;
-    }
-
+  int ser = 
+    meta.registerCalib("piano","mocha chip", "Test_Gen", "notAFile.xml",
+                       "XML", facilities::Timestamp("1990-1-5"),
+                       facilities::Timestamp("1995-6-11"), "", 
+                       "From add_meta test program in calibUtil package",
+                       "TEST", "SLAC", "", "INC", "", "");
+  if (ser) {
+    std::cout << "Successfully added metadata record; returned serial no. is " 
+              << ser << std::endl;
   }
+  else std::cout << "Failed to register new metadata record" << std::endl;
+
+
+  ser = meta.registerCalib("banjo","rocky road", "Test_Gen", 
+                           "notAnotherFile.xml",
+                           "XML", facilities::Timestamp("1997-1-5"), 
+                           facilities::Timestamp("1998-6-11"), "", 
+                           "From add_meta test program in calibUtil package",
+                           "TEST", "SLAC", "", "ILLEGAL", "", "");
+
+  if (ser) {
+    std::cout << "Successfully added metadata record; returned serial no. is " 
+              << ser << std::endl;
+  }
+  else std::cout << "Failed to register new metadata record" << std::endl;
+
+
 }
 
-// Get info needed to make a new record in the MySQL database interactively.
-// Some day might want to allow input from a file as well
-bool getUserInput(MetaInput* inp) {
-  using calibUtil::Metadata;
-
-  // Put in some defaults which user may override
-  inp->instr=Metadata::INSTLat;
-  inp->calibType=Metadata::CTYPE_TestGen;
-  inp->fmt = Metadata::FMTXml;
-  inp->cmpl = Metadata::CMPLOk;
-  inp->procLevel = Metadata::LEVELProd;
-  inp->flavor = "vanilla";
-  inp->inputDesc = "No real input description";
-  inp->notes = "No real notes";
-
-  std::cout << "Instrument: LAT (" << (int) Metadata::INSTLat << ") BTEM (";
-  std::cout << (int) Metadata::INSTBtem <<  ") BFEM (" 
-            << (int) Metadata::INSTBfem;
-  std::cout << ") EM (" << (int) Metadata::INSTEm << ")";
-  std::cout << "CU (" << (int) Metadata::INSTCu << ")"  << std::endl;
-  int inst;
-  std::cout << "Your type:  (or -1 to quit):  ";
-  std::cin >> inst;
-  if (inst < 0) return false;
-
-  inp->instr = (Metadata::eInstrument) inst;
-
-  std::cout << "Calib type is TestGen (" 
-            << (int) Metadata::CTYPE_TestGen << ")";
-  std::cout << std::endl;
-
-  std::cout << 
-    "Path to XML file, e.g. $(CALIBUTILROOT)/xml/test/gen1_1.xml (required): ";
-  std::cin >> inp->path;
-  std::string ascTime;
-  std::cout << 
-    "Validity interval start timestamp ending with *, e.g. 2003-01-12 08:00*";
-  std::cout << std::endl;
-
-  // std::cin always seems to have a \n at the front, even after
-  // std::cin.sync(), causing getline to come back immediately with no 
-  // data unless we use a non-standard character to signal end of input.
-  // std::cout << "Validity interval start timestamp, e.g. 2003-01-12 08:00" 
-  //     << std::endl;
-  //  std::cin.sync();
-  // std::getline(std::cin, ascTime);
-
-  std::getline(std::cin, ascTime, '*');
-  inp->vstart = facilities::Timestamp(ascTime);
-  std::cout << 
-    "Validity interval end timestamp ending with *, e.g. 2003-01-12 17:00*  :";
-  std::cout << std::endl;
-  std::getline(std::cin, ascTime, '*');
-  inp->vend = facilities::Timestamp(ascTime);
-  return true;
-}
-
-
-void gotIt(unsigned int ser, calibUtil::Metadata::eDataFmt dataFmt,
-           const std::string& fmtVersion, const std::string& dataIdent) {
-
-  std::cout << "Success reading info for record #" << ser << std::endl;
-  
-  std::cout << "Data format = " << (int) dataFmt << std::endl;
-  std::cout << "Format version = " << fmtVersion << std::endl;
-  std::cout << "Data ident = " << dataIdent << std::endl;
-}
 
 
--- src/test/test_meta.cxx
+++ src/test/test_meta.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/test/test_meta.cxx,v 1.7 2002/11/18 23:02:59 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/test/test_meta.cxx,v 1.8 2004/06/21 22:43:14 jrb Exp $
 /** @file test_meta.cxx
     Sample program to exercise calibration metadata database services
 */
@@ -7,36 +7,58 @@
 #include <iostream>
 #include "calibUtil/Metadata.h"
 
-void gotIt(unsigned int ser, calibUtil::Metadata::eDataFmt dataFmt,
+void gotIt(unsigned int ser, const std::string& dataFmt,
            const std::string& fmtVersion, const std::string& dataIdent);
 
-calibUtil::Metadata::eRet lookup(calibUtil::Metadata::eCalibType ctype,
+calibUtil::Metadata::eRet lookup(const std::string& ctype,
                                  const facilities::Timestamp& ts,
                                  unsigned int levelMask,
-                                 calibUtil::Metadata::eInstrument inst);
+                                 const std::string& inst);
+
+calibUtil::Metadata::eRet soonest(const std::string& ctype,
+                                  const facilities::Timestamp& update_start,
+                                  const facilities::Timestamp& update_end,
+                                  unsigned int levelMask,
+                                  const std::string& inst,
+                                  const std::string& flavor
+                                  = std::string("VANILLA"));
+
+void compatibility();
 
 int main(int, char**) {
   using calibUtil::Metadata;
   using facilities::Timestamp;
 
-  Metadata  meta("*", "*", "calib_test");
+  // just for exercise..
+  compatibility();
+
   Timestamp t_ok("2001-11-10 08:00");
   Timestamp t_none("2000-09-08 10:00");
+  Timestamp t_late("2010-12-19");
 
-  Metadata::eRet ret = lookup(Metadata::CTYPE_TKRDeadChan, t_ok, 
+  Metadata::eRet ret = lookup("TKR_DeadChan", t_ok, 
                               Metadata::LEVELDev | Metadata::LEVELTest, 
-                              Metadata::INSTBtem);
+                              "BTEM");
 
-  ret = lookup(Metadata::CTYPE_TKRDeadChan, t_ok, 
-               Metadata::LEVELProd | Metadata::LEVELDev, Metadata::INSTBtem);
+  ret = soonest("TKR_DeadChan", t_none, t_late, 
+                              Metadata::LEVELDev | Metadata::LEVELTest, 
+                              "BTEM");
 
-  ret = lookup(Metadata::CTYPE_ACDEff, t_ok, 
+  ret = soonest("TKR_DeadChan", t_none, t_late, 
+                              Metadata::LEVELDev | Metadata::LEVELTest, 
+                              "BTEM", "invisible");
+
+  ret = lookup("TKR_DeadChan", t_ok, 
+               Metadata::LEVELProd | Metadata::LEVELDev, "BTEM");
+
+  ret = lookup("ACD_Eff", t_ok, 
                Metadata::LEVELProd | Metadata::LEVELDev, 
-               Metadata::INSTBtem);
-  ret = lookup(Metadata::CTYPE_TKRHotChan, t_none, Metadata::LEVELDev,
-               Metadata::INSTBtem);
+               "BTEM");
+  ret = lookup("TKR_HotChan", t_none, Metadata::LEVELDev,
+               "BTEM");
 
   // Try to insert a record
+  /*
   ret = meta.openRecord(Metadata::INSTEm, Metadata::CTYPE_TKRHotChan,
                    Metadata::FMTXml, "1.0",
                    "$CALIBUTILROOT/xml/test/testHot-2002-05-02.xml",
@@ -72,35 +94,35 @@ int main(int, char**) {
     std::cout << "Successfully inserted new record, serial number " 
               << newSerial << std::endl;
   }
+  */
   return(ret);
 
 }
 
 
-void gotIt(unsigned int ser, calibUtil::Metadata::eDataFmt dataFmt,
+void gotIt(unsigned int ser, const std::string& dataFmt,
            const std::string& fmtVersion, const std::string& dataIdent) {
 
   std::cout << "Success reading info for record #" << ser << std::endl;
   
-  std::cout << "Data format = " << (int) dataFmt << std::endl;
+  std::cout << "Data format = " <<  dataFmt << std::endl;
   std::cout << "Format version = " << fmtVersion << std::endl;
   std::cout << "Data ident = " << dataIdent << std::endl;
 }
 
-calibUtil::Metadata::eRet lookup(calibUtil::Metadata::eCalibType ctype,
+calibUtil::Metadata::eRet lookup(const std::string& ctype,
                                  const facilities::Timestamp& ts,
                                  unsigned int levelMask,
-                                 calibUtil::Metadata::eInstrument inst) {
+                                 const std::string& inst) {
   using calibUtil::Metadata;
   unsigned int ser;
-
   
   std::cout << std::endl;
   std::cout << "lookup called with input " << std::endl;
-  std::cout << "   calibType = " << (int) ctype <<std::endl;
+  std::cout << "   calibType = " <<  ctype <<std::endl;
   std::cout << "   timestamp = " << ts.getString() << std::endl;
   std::cout << "   levelMask = " << levelMask << std::endl;
-  std::cout << "   instrument = " << (int) inst << std::endl;
+  std::cout << "   instrument = " << inst << std::endl;
 
   Metadata       meta("*", "*", "calib_test");
   Metadata::eRet ret = meta.findBest(&ser, ctype, ts, levelMask, inst);
@@ -113,7 +135,7 @@ calibUtil::Metadata::eRet lookup(calibUtil::Metadata::eCalibType ctype,
   }
   else {
     std::string  fmtVersion;
-    Metadata::eDataFmt dataFmt;
+    std::string dataFmt;
     std::string dataIdent;
 
     ret = meta.getReadInfo(ser, dataFmt, fmtVersion, dataIdent);
@@ -131,4 +153,64 @@ calibUtil::Metadata::eRet lookup(calibUtil::Metadata::eCalibType ctype,
   return ret;
 }
 
+calibUtil::Metadata::eRet soonest(const std::string& ctype,
+                                  const facilities::Timestamp& update_start,
+                                  const facilities::Timestamp& update_end,
+                                  unsigned int levelMask,
+                                  const std::string& inst,
+                                  const std::string& flavor) {
+  using calibUtil::Metadata;
+  unsigned int ser;
 
+  
+  std::cout << std::endl;
+  std::cout << "soonest called with input " << std::endl;
+  std::cout << "   calibType = " <<  ctype <<std::endl;
+  std::cout << "   update_start = " << update_start.getString() << std::endl;
+  std::cout << "   update_end = " << update_end.getString() << std::endl;
+  std::cout << "   levelMask = " << levelMask << std::endl;
+  std::cout << "   instrument = " << inst << std::endl;
+  std::cout << "   flavor     = " << flavor << std::endl;
+
+  Metadata       meta("*", "*", "calib_test");
+  Metadata::eRet ret = meta.findSoonAfter(&ser, ctype, &update_start, 
+                                          &update_end, levelMask, inst, flavor);
+
+  if (ret != Metadata::RETOk) {
+    std::cout << "findSoonAfter failed with status" << (int) ret << std::endl;
+  }
+  else if (!ser) {
+    std::cout << "Query succeeded; no rows found." << std::endl;
+  }
+  else {
+    std::string  fmtVersion;
+    std::string dataFmt;
+    std::string dataIdent;
+
+    ret = meta.getReadInfo(ser, dataFmt, fmtVersion, dataIdent);
+    
+    if (ret == Metadata::RETOk) { 
+      gotIt(ser, dataFmt, fmtVersion, dataIdent);
+    }
+
+    else {
+      std::cout << "Failed reading info for record #" << ser;
+      std::cout << " with code " << (int) ret << std::endl;
+    }
+  }
+
+  return ret;
+}
+
+void compatibility() {
+  using calibUtil::Metadata;
+
+  Metadata       meta("*", "*", "calib_test");
+  Metadata::eRet  ret = meta.compareSchema();
+
+  if (ret == Metadata::RETOk) {
+    std::cout << "Schema compatible with MySQL dbs " << std::endl;
+  } else {
+    std::cout << "Schema *NOT* compatible with MySQL dbs " << std::endl;
+  }
+}
